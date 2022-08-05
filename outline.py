65b9e234cdfa20b0e4a9f34c8b1a79b77ba999e1
KhoiMinhHa <98237955+KhoiMinhHa@users.noreply.github.com>
2022-08-04 21:21:12 -0400

    Update outline.py

--- outline.py
+++ outline.py
@@ -6,6 +6,24 @@
 def getJobList(role,location):
     url = f'https://www.talent.com/jobs?k={role}&l={location}'
     # Complete the missing part of this function here
+    response = requests.get(url)
+    print(response.status_code)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    JobDetails = soup.find_all('div', class_='card card__job')
+    #create an array here
+    my_array = []
+    for job in JobDetails:
+       jobTitle = job.find('h2', class_='card__job-title').text.strip()
+       company = job.find('div', class_='card__job-empname-label').text.strip()
+       description = job.find('p', class_='card__job-snippet').text.replace('\n', '').replace("'", "").strip()
+       jobDetailsjson = {
+           "Title": jobTitle,
+           "Company": company,
+           "Description": description
+       }
+       #Add jobDetailsjson to that array
+       my_array = [jobDetailsjson]
+    return my_array
 
 #save data in JSON file
 def saveDataInJSON(jobDetails):
